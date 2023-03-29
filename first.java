ccc2ba912b6e88473e4e21e537fb12696070cec3
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-29 08:42:59 +0000

    .

--- first.java
+++ first.java
@@ -1,5 +1,20 @@
+
+import java.util.Scanner;
+
 class first {
     public static void main(String args[]) {
-        System.out.println("this is a simple java program..");
+        System.out.println("\nthis is a simple java program..");
+        System.out.println("first.main()");
+        System.out.println("\n en the input: ");
+
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println("value of input is: " + x);
+
+        System.out.println("ebter a string type: ");
+        Scanner second = new Scanner(System.in);
+        String a = second.nextLine();
+        System.out.println("the string is: " + a + " pre val is" + x);
+
     }
 }
