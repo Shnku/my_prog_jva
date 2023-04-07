b9e3fc2d801d33cff9c6f4e679eff1aff887ab51
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-07 10:45:38 +0000

    .

--- first.java
+++ first.java
@@ -7,14 +7,22 @@ class first {
         System.out.println("first.main()");
         System.out.println("\n en the input: ");
 
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        System.out.println("value of input is: " + x);
+        try (Scanner in = new Scanner(System.in)) {
+            //Scanner in = new Scanner(System.in);
+            /*
+             * Resource leak: 'in' is never closedJava(536871799)
+                Scanner in - first.main(String[])
 
-        System.out.println("ebter a string type: ");
-        try (Scanner second = new Scanner(System.in)) {
-            String a = second.nextLine();
-            System.out.println("the string is: " + a + " pre val is" + x);
+                 quick fix.. surrounded by try{} block....
+             */
+            int x = in.nextInt();
+            System.out.println("value of input is: " + x);
+
+            System.out.println("ebter a string type: ");
+            try (Scanner second = new Scanner(System.in)) {
+                String a = second.nextLine();
+                System.out.println("the string is: " + a + " pre val is" + x);
+            }
         }
 
     }
