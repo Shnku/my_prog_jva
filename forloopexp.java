07f77b0cb446a3b81c0299f1d2d0a0fa7db925bc
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-05-27 17:31:28 +0000

    .

---
+++ forloopexp.java
@@ -0,0 +1,9 @@
+public class forloopexp {
+    public static void main(String[] args) {
+        System.out.println("Start.................");
+        for (int i = 0; i < 5;) {
+            System.out.println("this is end..");
+        }
+
+    }
+}
