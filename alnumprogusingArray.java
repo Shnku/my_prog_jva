d0343bc7984c7b196147f4575a88cc298f048b31
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-05-31 13:15:37 +0000

    .

--- alnumprogusingArray.java
+++ alnumprogusingArray.java
@@ -1,25 +1,26 @@
+import java.util.Scanner;
+
 public class alnumprogusingArray {
 
-    String nolist[] = { "perfectno", "krishnamurtyno", "armstrongno" };
+    static String nolist[] = { "perfectno", "krishnamurtyno", "armstrongno" };
 
-    static boolean cheaker(int no, String choice) {
+    static boolean cheaker(int no, int choice) {
         int copy = no;
         int rem;
         int sum = 0;
-        nolist key = nolist.valueOf(choice);
 
         while (copy != 0) {
             rem = copy % 10;
 
-            switch (key) {
+            switch (choice) {
 
-                case perfectno:
+                case 0:
                     sum += rem;
                     break;
-                case krishnamurtyno:
+                case 1:
                     sum += factorial(rem);
                     break;
-                case armstrongno:
+                case 2:
                     String n = String.valueOf(no);
                     int digit = n.length();
                     sum += Math.pow(rem, digit);
@@ -36,7 +37,23 @@ public class alnumprogusingArray {
     static int factorial(int rem) {
         return rem == 0 ? 1 : rem + factorial(rem - 1);
     }
-    public static void main(String[] a) {
 
+    public static void main(String[] a) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the number: ");
+        int no = sc.nextInt();
+
+        System.out.print("Available choices...: \n_");
+        for (String s : nolist) {
+            System.out.printf("|%s| ", s);
+        }
+
+        System.out.print("\nen yer choice: ");
+        short c = sc.nextShort();
+
+        System.out.print("is the no " + nolist[c - 1] + "?\t" + cheaker(no, c - 1));
+
+        sc.close();
     }
 }
